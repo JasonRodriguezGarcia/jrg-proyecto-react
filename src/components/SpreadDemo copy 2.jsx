7e1b8656415111792c05782953011bf1d674ddc1
JasonRodriguezGarcia <jasonrodriguezempleo.gmail.com>
2025-03-24 10:54:10 +0100

    cambio en SpreadDemo copy 2.jsx

--- src/components/SpreadDemo copy 2.jsx
+++ src/components/SpreadDemo copy 2.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 const SpreadDemo = () => {
 
     const [resultado, setResultado] = useState([1, 2, 3, 4, 0])
+    const [mensaje, setMensaje] = useState("")
 
     // setResultado(5) // esto es un correcto, se caen en un loop, cambia valor, renderiza, cambia valor, renderiza, ...
 
@@ -12,19 +13,30 @@ const SpreadDemo = () => {
         var nuevoArray = resultado
         // resultado.push(6)
         // setResultado(nuevoArray)// esto no funciona
-        setResultado(resultado => 
-            [...resultado, 6]
-            // usando resultado de antes a los que añado el 6
+      // Actualizamos el estado con el nuevo número (6)
+        setResultado((resultadoAnterior) => {
+            const nuevoArray = [...resultadoAnterior, 6];
+            const longitud = nuevoArray.length;
 
-        ) 
-
+            // Condición para actualizar el mensaje
+            if (longitud > 6) {
+                setMensaje("Más de 6");
+            } else {
+                setMensaje(""); // Limpiar el mensaje si no se supera el límite
+            }
+            return nuevoArray;
+        });
     }
+
     return (
         <>
             <h1>SpreadDemo</h1>
             <button onClick={agregar}>Agregar</button>
             {resultado.map(result => (
-                <p>Resultado: {result}</p>
+                <div>
+                    <p>Resultado: {result}</p>
+                    {mensaje && <p>Mensaje: {mensaje}</p>}
+                </div>
             ))}
         </>
     )
